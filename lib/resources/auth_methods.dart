9bd5b2683e478ee92176c27f8f8f45ca6535e207
Arethusa <arethusa.rayhan777@gmail.com>
2024-05-28 18:55:02 +0700

    Modelling User

--- lib/resources/auth_methods.dart
+++ lib/resources/auth_methods.dart
@@ -2,6 +2,7 @@ import 'dart:typed_data';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:friendszone_app/models/user.dart' as model;
 import 'package:friendszone_app/resources/storage_methods.dart';
 
 class AuthMethods {
@@ -21,16 +22,20 @@ class AuthMethods {
       UserCredential cred = await _auth.createUserWithEmailAndPassword(email: email, password: password);
       print(cred.user!.uid);
 
-      String photoUrl = await StorageMethods().uploadImageToStorage('profilePics', file, false);
-      await _firestore.collection('users').doc(cred.user!.uid).set({
-        'username': username,
-        'uid': cred.user!.uid,
-        'email': email,
-        'bio': bio,
-        'followers': [],
-        'following': [],
-        'photoUrl': photoUrl,
-      });
+      String photoUrl = await StorageMethods()
+      .uploadImageToStorage('profilePics', file, false);
+
+      model.User user = model.User(
+        username: username,
+        uid: cred.user!.uid,
+        email: email,
+        bio: bio,
+        photoUrl: photoUrl,
+        following: [],
+        followers: [],
+      );
+
+      await _firestore.collection('users').doc(cred.user!.uid).set(user.toJson(),);
 
       res = "success";
       }
