e1836d09e44a12d09777b6f7ab363a8e7931cd53
BenayaJuanda <107125249+BenayaJuanda@users.noreply.github.com>
2024-06-09 23:13:15 +0700

    Fixing Comments

--- lib/widgets/post_card.dart
+++ lib/widgets/post_card.dart
@@ -1,9 +1,12 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:friendszone_app/models/user.dart';
 import 'package:friendszone_app/providers/user_provider.dart';
 import 'package:friendszone_app/resources/firestore_methods.dart';
 import 'package:friendszone_app/screens/comments_screen.dart';
 import 'package:friendszone_app/utils/colors.dart';
+import 'package:friendszone_app/utils/utils.dart';
 import 'package:friendszone_app/widgets/like_animation.dart';
 import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
@@ -21,7 +24,25 @@ class PostCard extends StatefulWidget {
 
 class _PostCardState extends State<PostCard> {
   bool isLikeAnimating = false;
+  int commentLen = 0;
 
+  @override
+  void initState() {
+    super.initState();
+    getComments();
+  }
+
+  void getComments() async {
+   try {
+   QuerySnapshot snap = await FirebaseFirestore.instance.collection('posts').doc(widget.snap['postId']).collection('comments').get();
+   commentLen = snap.docs.length;
+    }catch(e){
+      showSnackBar(e.toString(),context);
+    }
+    setState((){
+
+    });
+  }
   @override
   Widget build(BuildContext context) {
     final User user = Provider.of<UserProvider>(context).getUser;
@@ -229,7 +250,7 @@ class _PostCardState extends State<PostCard> {
                   child: Container(
                     padding: const EdgeInsets.symmetric(vertical: 4),
                     child: Text(
-                      'View all 200 comments',
+                      'View all $commentLen comments',
                       style:
                           const TextStyle(fontSize: 16, color: secondaryColor),
                     ),
